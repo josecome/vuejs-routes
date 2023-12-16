3ece8a7a710ca7bf6139577cf32ec1de74c39c51
Jose Jaime Come <josejaimecome@gmail.com>
2023-12-16 13:06:10 +0200

    Route paths added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup>
+import { ref } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 const auth = useAuthStore()
+const route_name = ref('')
+const route_path = ref('')
 </script>
 <template>
   <main>
-    <h1>Home page</h1>    
+    <h1>Home page</h1><span>Route name: {{ route_name }}</span>  <span>Route name: {{ route_name }}</span>  <br />
+    <strong>List of Routes</strong><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
+    <RouterLink to="/" class="route">Route path: /</RouterLink><br />
   </main>
 </template>
+<style scoped>
+span {
+  padding: 8px;
+  margin: 8px;
+  font-size: 26px;
+}
+strong {
+  font-size: 26px;
+}
+.route {
+  padding: 6px;
+  margin: 6px;
+  font-size: 18px;
+}
+</style>
