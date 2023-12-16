99e87453a5b2ad567915a6fb52c975f9582076f3
Jose Jaime Come <josejaimecome@gmail.com>
2023-12-16 13:09:37 +0200

    Minor changes made

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 const auth = useAuthStore()
-const route_name = ref('')
-const route_path = ref('')
+const route = useRoute()
+const route_name = ref(route.name)
+const route_path = ref(route.path)
 </script>
 <template>
   <main>
-    <h1>Home page</h1><span>Route name: {{ route_name }}</span>  <span>Route name: {{ route_name }}</span>  <br />
+    <h1>Home page</h1><span>Route name: <strong>{{ route_name }}</strong></span>  <span>Route path: <strong>{{ route_path }}</strong></span>  <br />
     <strong>List of Routes</strong><br />
     <RouterLink to="/" class="route">Route path: /</RouterLink><br />
     <RouterLink to="/" class="route">Route path: /</RouterLink><br />
